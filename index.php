08cda1d134f66eb4e465b915c975d0908ef8c0de
Nils Brasz <nbrasz@splunk.com>
2016-03-03 15:40:49 -0800

    Test connection to Azure MySql

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+include('config.php');
 
-echo 'Hello World';
+echo 'Hello World<hr>';
+
+		$mysqli = new mysqli($host, $user, $pw, $dbname, 3306);
+
+		if(!$mysqli->connect_error)
+		{
+			echo 'DB Connect Success';
+			mysqli_close($mysqli);
+			
+		}
+		else
+		{
+			echo 'DB Connect fail';
+		}
 
 ?>
